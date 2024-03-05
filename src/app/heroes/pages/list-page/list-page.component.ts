967eb8601d159f93de740c907ba7a21d8f1cf685
Beatriz Cano <130498392+BeatrizCano@users.noreply.github.com>
2024-03-05 16:07:34 +0100

    add_pagination_and_change_autocomplete_for_select

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-list-page',
@@ -10,12 +11,25 @@ import { HeroesService } from '../../services/heroes.service';
 export class ListPageComponent implements OnInit {
 
   public heroes: Hero[] = [];
+  displayedHeroes: Hero[] = [];
+  pageSize: number = 8;
+  totalHeroes: number = 0;
 
   constructor( private heroesService: HeroesService ) {}
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-      .subscribe( heroes => this.heroes = heroes );
+      .subscribe( heroes => {
+        this.heroes = heroes;
+        this.totalHeroes = this.heroes.length;
+        this.onPageChange({ pageIndex: 0, pageSize: this.pageSize } as PageEvent);
+      });
+  }
+
+  onPageChange(event: PageEvent): void {
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
+    this.displayedHeroes = this.heroes.slice(startIndex, endIndex);
   }
 
 }
--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,39 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: ``
+
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit {
+  selectedHeroControl = new FormControl();
+  heroes: Hero[] = [];
 
-  public searchInput = new FormControl('');
-  public heroes: Hero[] = [];
-  public selectedHero?: Hero;
+  constructor(private heroesService: HeroesService) {}
 
-  constructor( private heroesService: HeroesService ) {}
-
-  searchHero() {
-    const value: string = this.searchInput.value || '';
-
-    this.heroesService.getSuggestion( value )
-      .subscribe( heroes => this.heroes = heroes );
+  ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe(heroes => {
+      this.heroes = this.sortHeroesAlphabetically(heroes);
+    });
   }
 
-  onSelectedOption( event: MatAutocompleteSelectedEvent ): void {
-    if( !event.option.value ) {
-      this.selectedHero = undefined;
-      return;
-    }
-
-    const hero: Hero = event.option.value;
-    this.searchInput.setValue( hero.superhero );
-
-    this.selectedHero = hero; 
+  sortHeroesAlphabetically(heroes: Hero[]): Hero[] {
+    return heroes.sort((a, b) => a.superhero.localeCompare(b.superhero));
   }
-
 }
